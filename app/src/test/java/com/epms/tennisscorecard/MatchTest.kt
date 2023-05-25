37f58514130ac4c8160eeb49f3320f0ffc74bc47
Etienne Païta <etienne@studio-equinoxe.com>
2023-05-25 10:36:09 +0200

    Refactor tests on Unit test package

--- app/src/test/java/com/epms/tennisscorecard/MatchTest.kt
+++ app/src/test/java/com/epms/tennisscorecard/MatchTest.kt
@@ -1,12 +1,9 @@
 package com.epms.tennisscorecard
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertThrows
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class MatchTest {
 
     @Test
--- app/src/test/java/com/epms/tennisscorecard/PlayerScoreTest.kt
+++ app/src/test/java/com/epms/tennisscorecard/PlayerScoreTest.kt
@@ -1,11 +1,8 @@
 package com.epms.tennisscorecard
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 internal class PlayerScoreTest {
 
     @Test
